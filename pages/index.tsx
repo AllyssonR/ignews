39ca4a715d9ea59a1046c7cdea0e1a8ec7c0ceb6
AllyssonR <42867875+AllyssonR@users.noreply.github.com>
2021-05-21 22:03:14 -0300

    start home page

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/home.module.scss";
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio|ig.news</title>
+      </Head>
+      <div className={styles.title}>
+        hello <span>world</span>
+      </div>
+    </>
+  );
+}
